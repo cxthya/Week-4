48e7a83988e2fafb1ce7ca12011697fac30f38ee
cxthya <calderonhernandezcinthya@gmail.com>
2026-02-17 17:08:13 -0500

    Display User Name and Age
    Displaying anem and age in the same line.

---
+++ assignment 2 display_user.py
@@ -0,0 +1,11 @@
+'''
+Cinthya Calderon-Hernandez
+CMSC 111
+Spring 2026
+Assignment 2: Display User Information
+'''
+
+def display_user(name, age):
+    print(f"{name} is {age} years old.")
+#printing out in a single line 
+display_user("John", 25)
